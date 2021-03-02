47376f3b75cdb583b32f11b5a1dce806dc8fef1f
Ronan <liruonan@hotmail.com>
2021-03-02 23:37:30 +0800

    update(roMergeRequest): deal

--- src/cmds/roMergeRequest.ts
+++ src/cmds/roMergeRequest.ts
@@ -2,41 +2,39 @@ import * as vscode from 'vscode';
 // serive
 import { gitLocalOrigin, gitRemoteV, gitBranchCurrent } from '../cli-services/gitService';
 // utils
-import { showInfo } from '../utils/showMsg';
+import { showError, showInfo, showWarning } from '../utils/showMsg';
 
 export default async function helloWorld (context: vscode.ExtensionContext) {
-  const testPath = context.extensionPath;
+  const folders = vscode.workspace.workspaceFolders;
+  if (!Array.isArray(folders)) {
+    showWarning('unexpect: ', JSON.stringify(folders));
+    return;
+  }
+  if (folders.length < 1) {
+    showWarning('没找到工作区');
+    return;
+  }
+
+  const curFolder = folders[0];
+  const curPath = curFolder.uri.fsPath;
 
   try {
-    const { stdout } = await gitBranchCurrent(testPath) as any;
+    const { stdout } = await gitBranchCurrent(curPath) as any;
     const curBranchName = (stdout + '').trim();
 
-    const res = await gitLocalOrigin(testPath);
+    const res = await gitLocalOrigin(curPath);
     const url = `${res}`.replace('.git', '/merge_requests/new?') + 'merge_request%5Bsource_branch%5D=' + curBranchName;
 
     require('shelljs')
-      .exec('start ' + url, (err: any, stdout: any, stderr: any) => {
-        showInfo('Hi, ro mr 我要疯了');
+      .exec('open ' + url, (code: number, stdout: any, stderr: any) => {
+        if (code === 0) {
+          showInfo('Ro: mr/pr', ' url 已经在Browser打开');
+        } else {
+          showError('Ro: mr/pr', stderr);
+        }
+        console.log(code, stdout, stderr);
       });
   } catch (error) {
     console.log(error);
   }
-
-  // console.log(context);
-  const cp = require('child_process');
-
-  cp.exec('git remote -v',
-    { cwd: testPath, silent: true },
-    (err: any, stdout: any, stderr: any) => {
-      console.log(1, err);
-      console.log(2, stdout);
-      console.log(3, stderr);
-    }
-  );
-
-
-
-  // cp.exec('start https://www.baidu.com', (err: any, stdout: any, stderr: any) => {
-  //   vscode.window.showInformationMessage('Hi, ro mr 我要疯了');
-  // });
 }
